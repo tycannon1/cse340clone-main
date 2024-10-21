891563af2205d2b0dba4a409633618fa0f7f69ba
Ty Cannon <cannont41@gmail.com>
2024-10-20 20:51:34 -0600

    database assignment2

---
+++ database/assignment2.sql
@@ -0,0 +1,22 @@
+INSERT INTO account (first_name, last_name, email, password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+UPDATE account
+SET account_type = 'Admin'
+WHERE email = 'tony@starkent.com';
+
+DELETE FROM account
+WHERE email = 'tony@starkent.com';
+
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+SELECT inventory.inv_make, inventory.inv_model, classification.classification_name
+FROM inventory
+INNER JOIN classification ON inventory.classification_id = classification.classification_id
+WHERE classification.classification_name = 'Sport';
+
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
