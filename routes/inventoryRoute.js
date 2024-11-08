12326e0430f18662575245d1ba36f1d0acaabac8
Ty Cannon <cannont41@gmail.com>
2024-11-08 13:05:08 -0800

    w6 requirements

---
+++ routes/inventoryRoute.js
@@ -0,0 +1,19 @@
+// // Needed Resources 
+// const express = require("express")
+// const router = new express.Router() 
+// const invController = require("../controllers/invController")
+
+// // Route to build inventory by classification view
+// router.get("/type/:classificationId", invController.buildByClassificationId);
+
+// module.exports = router;
+
+const express = require("express")
+const router = new express.Router() 
+const invController = require("../controllers/invController")
+const invCont = require("../controllers/invController");
+
+// Route to display inventory items by classification
+router.get("/type/:classificationId", invController.buildByClassificationId)
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,3 +1,62 @@
+// /* ******************************************
+//  * This server.js file is the primary file of the 
+//  * application. It is used to control the project.
+//  *******************************************/
+// /* ***********************
+//  * Require Statements
+//  *************************/
+// const express = require("express")
+// const expressLayouts = require("express-ejs-layouts")
+// const env = require("dotenv").config()
+// const db = require('./config/db') // Add this line for database connection
+// const app = express()
+// const static = require("./routes/static")
+
+// const inventory = require('./routes/inventory');
+// app.use(inventory);
+
+// const baseController = require("./controllers/baseController")
+
+// /* ***********************
+//  * Serve Static Files
+//  *************************/
+// app.use(express.static("public"))
+
+// /* ***********************
+//  * View Engine and Templates
+//  *************************/
+// app.set("view engine", "ejs")
+// app.use(expressLayouts)
+// app.set("layout", "./layouts/layout") // not at views root
+
+// /* ***********************
+//  * Routes
+//  *************************/
+// app.use(static)
+
+// // Index route
+// // app.get("/", function(req, res) {
+// //   res.render("index", {title: "Home"})
+// // })
+// app.get("/", baseController.buildHome)
+
+// // Inventory routes
+// app.use("/inv", inventoryRoute)
+
+// /* ***********************
+//  * Local Server Information
+//  * Values from .env (environment) file
+//  *************************/
+// const port = process.env.PORT
+// const host = process.env.HOST
+
+// /* ***********************
+//  * Log statement to confirm server operation
+//  *************************/
+// app.listen(port, () => {
+//   console.log(`app listening on ${host}:${port}`)
+// })
+
 /* ******************************************
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
@@ -13,7 +72,7 @@ const app = express()
 const static = require("./routes/static")
 
 const inventory = require('./routes/inventory');
-app.use(inventory);
+const baseController = require("./controllers/baseController")
 
 /* ***********************
  * Serve Static Files
@@ -31,11 +90,13 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+const inventoryRoute = require('./routes/inventoryRoute');
 
 // Index route
-app.get("/", function(req, res) {
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+
+// Inventory routes
+app.use("/inv", inventoryRoute);
 
 /* ***********************
  * Local Server Information
